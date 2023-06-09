b542d7f05faffadd7752b00ba631c4574652df3e
Vansh Marda <121536612+VanshMarda@users.noreply.github.com>
2023-06-09 16:24:07 +0530

    Uploaded via terminal

---
+++ react-graphql/README.md
@@ -0,0 +1,16 @@
+Graphql + React [Project](https://graphql-react-example.netlify.app/)
+----
+
+Make query `GetExchangeRates` here https://48p1r2roz4.sse.codesandbox.io
+```
+query GetExchangeRates {
+    rates(currency: "USD") {
+      currency
+      rate
+    }
+  }
+```  
+
+![gif](gif.gif)
+
+---------
---
+++ react-graphql/src/App.tsx
@@ -0,0 +1,38 @@
+import React from 'react';
+import './App.css';
+import {gql, useQuery} from "@apollo/client";
+
+const EXCHANGE_RATES = gql`
+  query GetExchangeRates {
+    rates(currency: "USD") {
+      currency
+      rate
+    }
+  }
+`;
+
+function ExchangeRates() {
+    const {loading, error, data} = useQuery(EXCHANGE_RATES);
+
+    if (loading) return <p>Loading...</p>;
+    if (error) return <p>Error :(</p>;
+
+    return data.rates.map(({currency, rate}: { currency: string, rate: number }) => (
+        <div key={currency}>
+            <p>
+                {currency}: {rate}
+            </p>
+        </div>
+    ));
+}
+
+function App() {
+    return (
+        <div>
+            <h2>Currency Exchange Rates</h2>
+            <ExchangeRates/>
+        </div>
+    );
+}
+
+export default App;
